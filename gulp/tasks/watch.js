2186590ede47abbec31c9e6837a575234af8eb15
Michel Hofmann <3rdgeist@gmail.com>
2014-12-31 21:10:11 +0100

    removed existing project structure

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,11 +3,11 @@
    - gulp/tasks/browserSync.js watches and reloads compiled files
 */
 
-var gulp  = require('gulp');
-var config= require('../config');
+var gulp = require('gulp');
+var config = require('../config');
 
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-  gulp.watch(config.sass.src,   ['sass']);
-  gulp.watch(config.images.src, ['images']);
-  gulp.watch(config.markup.src, ['markup']);
+    gulp.watch(config.sass.src, ['sass']);
+    gulp.watch(config.images.src, ['images']);
+    gulp.watch(config.jade.src, ['jade']);
 });
