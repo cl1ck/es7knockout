67cb8ef9383dc79e5d677f1a707f0c6d6455b6ee
Michel Hofmann <3rdgeist@gmail.com>
2015-03-03 00:04:20 +0100

    fixed config errors

--- gulp/config.js
+++ gulp/config.js
@@ -81,5 +81,5 @@ module.exports = {
     tests: {
         src: src + '/test/**/*.spec.js',
         karmaConfig: __dirname + '/../karma.conf.js'
-    },
+    }
 };
--- src/config.js
+++ src/config.js
@@ -8,4 +8,3 @@ System.config({
   }
 });
 
-document.
