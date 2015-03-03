1d1c2fb713c8f7e1e0938adb7c8d1a07737750fb
Michel Hofmann <3rdgeist@gmail.com>
2015-03-03 11:05:22 +0100

    added sourcemaps to javascript dist task

--- gulp/tasks/javascript-dist.js
+++ gulp/tasks/javascript-dist.js
@@ -2,10 +2,11 @@
 
 var gulp            = require('gulp');
 var config          = require('../config').javascript;
-var uglify          = require('gulp-uglifyjs');
+var uglify          = require('gulp-uglify');
 var shell           = require('gulp-shell');
 var runSequence     = require('run-sequence');
 var del             = require('del');
+var concat          = require('gulp-concat');
 
 gulp.task('jspm:compile',  shell.task(
     ['jspm bundle-sfx ' + config.bundleMain + ' ' + config.dist + '/main.js']
@@ -13,7 +14,10 @@ gulp.task('jspm:compile',  shell.task(
 
 gulp.task('jspm:uglify', function() {
     return gulp.src([config.es6runtime, config.dist + '/main.js'])
-        .pipe(uglify('app.js', config.uglifySettings))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify(config.uglifySettings))
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.dist));
 });
 
