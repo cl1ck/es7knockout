84f26215fe121026e92f9ba0a3131e387f66b6af
Michel Hofmann <3rdgeist@gmail.com>
2015-05-11 12:00:35 +0200

    updated dependencies

--- src/jspm_config.js
+++ src/jspm_config.js
@@ -18,6 +18,8 @@ System.config({
     "babel": "npm:babel-core@5.2.15",
     "babel-runtime": "npm:babel-runtime@5.2.15",
     "core-js": "npm:core-js@0.9.6",
+    "knockout": "github:knockout/knockout@3.3.0",
+    "systemjs/plugin-text": "github:systemjs/plugin-text@0.0.2",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
