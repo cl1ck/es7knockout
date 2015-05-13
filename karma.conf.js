1980fdbea0f41903fb5ea174788d7e925f124129
Michel Hofmann <3rdgeist@gmail.com>
2015-05-13 17:06:53 +0200

    coverage

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'build/js/**/*.js': ['babel', 'coverage']
+            'src/js/**/*.js': ['babel', 'coverage']
         },
 
         babelPreprocessor: {
