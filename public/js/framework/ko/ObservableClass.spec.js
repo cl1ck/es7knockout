52a5612edbf8dcf36ed258b6d6a1148e2bc430b0
Michel Hofmann <3rdgeist@gmail.com>
2015-05-18 21:36:31 +0200

    100% coverage for observableCLass

--- public/js/framework/ko/ObservableClass.spec.js
+++ public/js/framework/ko/ObservableClass.spec.js
@@ -22,6 +22,18 @@ class ImportTest extends ObservableClass {
     get computedProperty() {
         return this.stringProperty + this.stringProperty;
     }
+
+    set computedProperty(newValue) {
+        this.stringProperty = newValue;
+    }
+
+    get computedNoSetter() {
+        return this.intProperty * 2;
+    }
+
+    testFunc() {
+        return this;
+    }
 }
 
 describe('ObservableClass', function() {
@@ -69,9 +81,33 @@ describe('ObservableClass', function() {
 
     it('should import getters as pureComputed', () => {
         let importTest = new ImportTest();
-        assert.equal(importTest.computedProperty, 'valuevalue');
         assert.isTrue(ko.isComputed(importTest.$computedProperty));
+        assert.isTrue(ko.isComputed(importTest.$computedNoSetter));
+    });
+
+    it('should update pureComputed when depending observables change', () => {
+        let importTest = new ImportTest();
+        assert.equal(importTest.computedProperty, 'valuevalue');
         importTest.stringProperty = 'test';
         assert.equal(importTest.computedProperty, 'testtest');
-    })
+        assert.equal(importTest.computedNoSetter, 6);
+        importTest.intProperty = 50;
+        assert.equal(importTest.computedNoSetter, 100);
+    });
+
+    it('should allow setters for writable pureComputed', () => {
+        let importTest = new ImportTest();
+        importTest.computedProperty = 'new';
+        assert.equal(importTest.computedProperty, 'newnew');
+    });
+
+    it('should throw an exception when assigning a value to readonly pureComputed', () => {
+        let importTest = new ImportTest();
+        assert.throws(() => importTest.computedNoSetter = 'test');
+    });
+
+    it('should auto-bind functions to the Component', () => {
+        let importTest = new ImportTest();
+        assert.equal(importTest.testFunc.apply(window), importTest);
+    });
 });
